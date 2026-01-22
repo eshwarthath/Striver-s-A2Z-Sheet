718df500153405617beaaad7514da705b358e07d
Eshwar Thath K R <eshwarthath@gmail.com>
2026-01-23 01:34:16 +0530

    Create Input_Output.cpp

---
+++ Input_Output.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void printNumber() {
+        int N;
+        cin>>N;
+        cout<<N<<endl;
+    }
+};
+
+int main()
+{
+    Solution s;
+    s.printNumber();
+
+    return 0;
+}
+
+
+
